177f4da01d85602c3a983ed485e0c31c9773be95
cachinguel3 <edu.crecenzio.1003@gmail.com>
2026-07-26 20:40:50 -0300

    refactor: change the directory name

---
+++ api/src/main/java/com/mineapi/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.mineapi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
+public class Test {
+
+  public static void main
+}
